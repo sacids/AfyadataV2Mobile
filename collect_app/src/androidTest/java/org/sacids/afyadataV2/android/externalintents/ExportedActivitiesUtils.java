b077a2c499d2f59adea6e34c25f1595c186abb17
Renfrid Ngolongolo <renfridfrancis@gmail.com>
2017-09-11 17:39:34 +0300

    Change package name to org.sacids.afyadataV2

---
+++ collect_app/src/androidTest/java/org/sacids/afyadataV2/android/externalintents/ExportedActivitiesUtils.java
@@ -0,0 +1,42 @@
+package org.sacids.afyadataV2.android.externalintents;
+
+import junit.framework.Assert;
+
+import java.io.File;
+
+import timber.log.Timber;
+
+import static org.sacids.afyadataV2.application.Collect.CACHE_PATH;
+import static org.sacids.afyadataV2.application.Collect.FORMS_PATH;
+import static org.sacids.afyadataV2.application.Collect.INSTANCES_PATH;
+import static org.sacids.afyadataV2.application.Collect.METADATA_PATH;
+import static org.sacids.afyadataV2.application.Collect.ODK_ROOT;
+import static org.sacids.afyadataV2.application.Collect.OFFLINE_LAYERS;
+
+class ExportedActivitiesUtils {
+
+    private static final String[] DIRS = new String[]{
+            ODK_ROOT, FORMS_PATH, INSTANCES_PATH, CACHE_PATH, METADATA_PATH, OFFLINE_LAYERS
+    };
+
+    static void clearDirectories() {
+        for (String dirName : DIRS) {
+            File dir = new File(dirName);
+            if (dir.exists()) {
+                if (dir.delete()) {
+                    Timber.i("Directory was not deleted");
+                }
+            }
+        }
+
+    }
+
+    static void testDirectories() {
+        for (String dirName : DIRS) {
+            File dir = new File(dirName);
+            Assert.assertTrue("File " + dirName + "does not exist", dir.exists());
+            Assert.assertTrue("File" + dirName + "does not exist", dir.isDirectory());
+        }
+    }
+
+}
---
+++ collect_app/src/androidTest/java/org/sacids/afyadataV2/android/externalintents/MainMenuActivityTest.java
@@ -0,0 +1,27 @@
+package org.sacids.afyadataV2.android.externalintents;
+
+import android.support.test.filters.Suppress;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.sacids.afyadataV2.activities.MainMenuActivity;
+
+import java.io.IOException;
+
+import static org.sacids.afyadataV2.android.externalintents.ExportedActivitiesUtils.testDirectories;
+
+@Suppress
+// Frequent failures: https://github.com/opendatakit/collect/issues/796
+public class MainMenuActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainMenuActivity> mainMenuActivityRule =
+            new ExportedActivityTestRule<>(MainMenuActivity.class);
+
+    @Test
+    public void mainMenuActivityMakesDirsTest() throws IOException {
+        testDirectories();
+    }
+
+}
