e9d97fc33067b8badc434159282a639494f21e74
Renfrid Ngolongolo <renfridfrancis@gmail.com>
2018-07-25 10:45:52 +0300

    Change folder path from odk to afyadata

--- collect_app/src/main/java/org/sacids/app/afyadata/application/Collect.java
+++ collect_app/src/main/java/org/sacids/app/afyadata/application/Collect.java
@@ -67,7 +67,7 @@ public class Collect extends Application {
 
     // Storage paths
     public static final String ODK_ROOT = Environment.getExternalStorageDirectory()
-            + File.separator + "odk";
+            + File.separator + "afyadata";
     public static final String FORMS_PATH = ODK_ROOT + File.separator + "forms";
     public static final String INSTANCES_PATH = ODK_ROOT + File.separator + "instances";
     public static final String CACHE_PATH = ODK_ROOT + File.separator + ".cache";
